08d5ee81538cb29779c693f55f7f8f5b31a688a7
nsantti <nsantti@nmu.edu>
2018-11-10 09:34:54 -0500

    Got basic UI designed

--- pub/client.js
+++ pub/client.js
@@ -1,11 +1,13 @@
-
+// Takes a word, returns a string all lowercase separated by underscores
 function cleanString(str) {
    return str.split(' ').filter(item => item.length > 0).map(word => word.toLowerCase()).join('_');
 }
 
+// Takes a lowercase word separated by underscores.
+// Returns a string with spaces instead of underscores, with the first
+// letter of each word capitalized
 function retrieve(str) {
    return str.split('_').map(capitalizeFirst).join(' ');
-
 }
 
 function capitalizeFirst(word) {
