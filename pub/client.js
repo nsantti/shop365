da514750e571ff62faea14200f787230d75147eb
ryanSalo <44672889+ryanSalo@users.noreply.github.com>
2018-11-15 17:09:10 -0500

    Update client.js

--- pub/client.js
+++ pub/client.js
@@ -1,13 +1,39 @@
 
-
 var socket = io();
 
 socket.on("displayItemFromServer", function (name, quantity, comment, priority) {
     console.log("NAME: " + name + "\nQUANTITY: " + quantity + "\nCOMMENT: " + comment + "\nPRIORITY: " + priority);
 });
 
+socket.on("getItemList", function (items) {
+    console.log(items);
+});
+
+/*socket.on("updateItemList", function (items) {
+    $("#groupID").text(retrieve(items[0].groupid));
+
+    let d = new Date();
+    $("#date").text(formatDate(d));
+
+    let i;
+    for (i of items) {
+        var h = $("<tr><td><input type='checkbox'/></td><td>" + retrieve(i.name) + "</td><td>" + i.quantity + "</td><td><input type='button' id='comments' value='Show Comments'/></td><td><input type='button' value='Menu'/></td></tr>");
+        $("#itemList").append(h);
+    }
+    //console.log(items);
+});*/
+
 function startItAll() {
+
+    //changeGroupModal
+    //generateGroupButton
+    //createGroupButton
+
     socket.emit("getAllItems");
+    socket.emit("getGroupItems");
+
+    $("#changeGroupModal").show();
+    $("#mainView").hide();
     $("#addItemModal").hide();
 
     $("#addItemButton").click(function () {
@@ -18,12 +44,10 @@ function startItAll() {
     $("#addItemCancel").click(function () {
         $("#mainView").show();
         $("#addItemModal").hide();
+        clearAllInputFields();
     });
 
-    //Ryan TODO: validate input before sending to server
     $("#modalItemSubmit").click(function () {
-        $("#mainView").show();
-        $("#addItemModal").hide();
         if (!validateName($("#modalItemName").val())) {
             //TODO: Handle bad name input
         }
@@ -32,26 +56,29 @@ function startItAll() {
         }
         else {
             socket.emit("receiveItemFromClient",
-                $("#modalItemName").val(),
+                cleanString($("#modalItemName").val()),
                 $("#modalItemQuantity").val(),
                 $("#modalItemComment").val(),
-                $("#modalItemPriority").val()
+                getModalItemPriority()
             );
+            $("#mainView").show();
+            $("#addItemModal").hide();
+            clearAllInputFields();
         }
     });
 }
 
-socket.on("getItemList", function (items) {
-    console.log(items);
-});
-
 $(startItAll);
 
+function getModalItemPriority() {
+    return $("#modalItemPriority").prop('checked');
+}
 
-
-// Takes a word, returns a string all lowercase separated by underscores
-function cleanString(str) {
-    return str.split(' ').filter(item => item.length > 0).map(word => word.toLowerCase()).join('_');
+function clearAllInputFields() {
+    $("#modalItemName").val("");
+    $("#modalItemQuantity").val(1);
+    $("#modalItemComment").val("");
+    $("#modalItemPriority").prop('checked', false);
 }
 
 function validateName(name) {
@@ -60,13 +87,21 @@ function validateName(name) {
 
 function validateQuantity(quantity) {
     let str = quantity.toString();
+    //console.log("STEP 1: " + str);
     let q = str.replace(/\s/g, '');
+    //console.log("STEP 2: " + q);
     if (q.length < 1) return false;
     for (let i = 0; i < q.length; i++) {
         if (q[i] < '0' || q[i] > '9') return false;
     }
-    //if (q.parseInt() < 1) return false;
+    //console.log("STEP 3: " + parseInt(q));
+    if (parseInt(q) < 1) return false;
     return true;
+}
+
+// Takes a word, returns a string all lowercase separated by underscores
+function cleanString(str) {
+    return str.split(' ').filter(item => item.length > 0).map(word => word.toLowerCase()).join('_');
 }
 
 // Takes a lowercase word separated by underscores.
@@ -105,5 +140,3 @@ exports.retrieve = retrieve;
 exports.makeDate = makeDate;
 exports.formatDate = formatDate;
 exports.dateMillis = dateMillis;
-exports.validateName = validateName;
-exports.validateQuantity = validateQuantity;
