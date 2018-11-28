b4e416c9dbab066cdb14d22f1e2c16a813e6d4a0
Ryan Salo <rsalo@nmu.edu>
2018-11-27 20:48:08 -0500

    Nate's Code

--- pub/client.js
+++ pub/client.js
@@ -1,23 +1,56 @@
 var socket = io();
 
-var group = "test_group";
+var allGroups = [];
+
+let group = "no_group_found";
+
+var clientItemArray = [];
 
 var currentItem;
 
-/*socket.on("displayItemFromServer", function (name, quantity, comment, priority) {
-    console.log("NAME: " + name + "\nQUANTITY: " + quantity + "\nCOMMENT: " + comment + "\nPRIORITY: " + priority);
-});*/
+var purchasedCount = 0;
 
-socket.on("updateItemList", function (items) {
+socket.on()
+
+socket.on("updateGroupList", function (groupArrayFromServer) {
+    allGroups = groupArrayFromServer;
+    //console.log(groupArrayFromServer);
+    // console.log(allGroups);
+    //console.log(allGroups[0].groupid);
+    $("#groupSelector").html("");
+
+    if (allGroups.length != 0) {
+        $('#createGroupButton').prop('disabled', false).css('opacity', 1);
+        var z;
+        for (z of allGroups) {
+            let g = z;
+            $('#groupSelector').append('<option value=' + g.name + '>' + retrieve(g.name) + '</option>');
+        }
+    }
+    else {
+        $('#createGroupButton').prop('disabled', true).css('opacity', 0.5);
+        $('#groupSelector').append('<option>No Groups Available</option>');
+    }
+});
+
+var currentItem;
+
+var items = [];
+
+var sortingType = {
+    quantity: 0,
+    name: 1,
+    priority: 2,
+    none: 3
+}
+
+var currentSort;
+
+function updateGUI(arr) {
     $("#table-body").html("");
-
-    //$("#groupID").text(retrieve(items[0].groupid));
-
-    $("#groupID").text(retrieve(group));
-
-    $("#date").text(formatDate(new Date()));
-
-    var i;
+    items = sortList(arr);
+    purchasedCount = 0;
+    $("#content").height(400 + 53*items.length);
     for (i of items) {
         let t = i;
         var h = $("<tr id='" + t._id + "' class='table-item'><td></td><td class='" + t._id + "'>" + retrieve(t.name) + "</td><td class='" + t._id + "'>" + t.quantity + "</td><td class='" + t._id + "'>" + t.comments + "</td><td></td></tr>");
@@ -29,7 +62,7 @@ socket.on("updateItemList", function (items) {
 
 
         pb.click(function () {
-            socket.emit("togglePriority", t._id, t.priority);
+            socket.emit("togglePriority", group, t._id, t.priority);
             console.log(t.name + " " + t.priority);
         });
 
@@ -39,12 +72,13 @@ socket.on("updateItemList", function (items) {
 
         editb.click(function () {
             $("#storeItemID").val(t._id);
-            $("#editModalItemName").val(t.name);
+            $("#editModalItemName").val(retrieve(t.name));
             $("#editModalItemQuantity").val(t.quantity);
             $("#editModalItemComment").val(t.comments);
             $("#editModalItemPriority").prop('checked', t.priority);
             $("#mainView").hide();
             $("#editItemModal").show();
+            $("#editModalItemName").focus();
         });
 
         delb.click(function () {
@@ -54,29 +88,55 @@ socket.on("updateItemList", function (items) {
         $("#table-body").append(h);
 
         if (t.purchased == true) {
-            console.log("Changing background color to green");
+            purchasedCount++;
+            //console.log("Changing background color to gray");
             $("#" + t._id).css("background-color", "#7c7c7c");
-        }
-        else {
-            console.log("Changing the background color to blue");
+        } else {
+            // console.log("Changing the background color to blue");
             $("#" + t._id).css("background-color", "#90AFC5");
         }
 
         $("." + t._id).click(function () {
-            console.log("The item you are sending to the server is " + t.name + " and the purchased boolean is " + t.purchased);
-            socket.emit("togglePurchased", t._id, t.purchased);
+            //console.log("The item you are sending to the server is " + t.name + " and the purchased boolean is " + t.purchased);
+            console.log("Toggling purchased");
+            socket.emit("togglePurchased", group, t._id, t.purchased);
         });
 
     }
-
+    console.log(purchasedCount);
+    //purchasedCount = 0;
     updateClickHandlers();
+}
+
+socket.on("updateItemList", function (items) {
+    $("#table-body").html("");
+    $("#groupID").text(retrieve(group).toUpperCase());
+    $("#date").text(makeDate());
+    console.log("Item array coming from server")
+    console.log(items);
+    updateGUI(items);
+});
+
+socket.on("forceClientCall", function (w) {
+    //console.log("forceClientCall");
+    socket.emit("getGroupItems", cleanString(group));
+});
+
+socket.on("forceOutOfList", function (w) {
+    $("#changeGroupModal").show();
+    $("#mainView").hide();
+    $("#addItemModal").hide();
+    $("#confirmDeleteAllModal").hide();
+    //alert("The group has been deleted by another member");
 });
 
 //Nate
 function updateClickHandlers() {
     $(".delete-button").click(function (event) {
         $("#confirmDeleteModal").show();
+        $("#addItemModal").hide();
         $("#mainView").hide();
+        $("#editItemModal").hide();
         $("#deleteItemName").text(retrieve(currentItem.name));
     });
 
@@ -88,23 +148,92 @@ function updateClickHandlers() {
     $("#confirmDeleteItemButton").click(function () {
         $("#mainView").show();
         $("#confirmDeleteModal").hide();
-        socket.emit("deleteItem", currentItem._id);
+        socket.emit("deleteItem", group, currentItem._id);
+    });
+    //Nick added these
+    $("#cancelDeleteAllItemsButton").click(function () {
+        $("#mainView").show();
+        $("#confirmDeleteAllModal").hide();
     });
 
+    $("#confirmDeleteAllItemsButton").click(function () {
+        $("#mainView").show();
+        $("#confirmDeleteAllModal").hide();
+        socket.emit("removePurchased", group);
+    });
+
+    $("#cancelDeleteGroupButton").click(function() {
+        $("#mainView").show();
+        $("#confirmDeleteGroupModal").hide();
+    });
+
+    $("#confirmDeleteGroupButton").click(function() {
+        socket.emit("deleteGroup", cleanString(group));
+        $("#changeGroupModal").show();
+        $("#mainView").hide();
+        $("#confirmDeleteGroupModal").hide();
+    });
+
+    $("#deleteGroupButton").click(function () {
+        $("#deleteGroupName").text(retrieve(group));
+        $("#mainView").hide();
+        $("#confirmDeleteGroupModal").show();
+    });
+
+    $("#cancelCreateNewGroupButton").click(function() {
+        $("#createNewGroupModal").hide();
+        $("#changeGroupModal").show();
+        $("#validateNewGroupDiv").hide();
+    });
+
+    
+}
+
+//Nate
+function sortList(arr) {
+    if (currentSort == sortingType.quantity) {
+        console.log("SORTING BY QUANTITY");
+        arr = arr.sort(function (a, b) {
+            if (a.quantity < b.quantity) return 1;
+            return -1;
+        });
+    } else if (currentSort == sortingType.name) {
+        arr = arr.sort(function (a, b) {
+            if (a.name > b.name) return 1;
+            return -1;
+        });
+    } else if (currentSort == sortingType.priority) {
+        arr = arr.sort(function (a, b) {
+            if (b.priority && !a.priority) return 1;
+            return -1;
+        })
+    }
+    return arr;
 }
 
 function startItAll() {
 
-    socket.emit("getAllItems");
-    socket.emit("getGroupItems");
+    // socket.emit("getGroups");
+    socket.emit("getGroupCollections");
+
+    if (typeof (group) === 'undefined') { //All items are loaded and then filtered when group is specified
+        //socket.emit("getAllItems");
+        console.log("ERRORR ERRORR EORR")
+    } else {
+        socket.emit("getGroupItems", group);
+    }
 
     $("#changeGroupModal").show();
     $("#mainView").hide();
     $("#addItemModal").hide();
+    $("#confirmDeleteAllModal").hide();
+    $("#changeGroupText").focus();
+    $("#changeGroupText").val('');
 
     $("#addItemButton").click(function () {
         $("#mainView").hide();
         $("#addItemModal").show();
+        $("#modalItemName").focus();
     });
 
     $("#addItemCancel").click(function () {
@@ -123,11 +252,11 @@ function startItAll() {
         }
         else {
             socket.emit("receiveItemFromClient",
-                group,
+                cleanString(group),
                 cleanString($("#modalItemName").val()),
                 $("#modalItemQuantity").val(),
                 $("#modalItemComment").val(),
-                getModalItemPriority()
+                $("#modalItemPriority").is(":checked")
             );
             $("#mainView").show();
             $("#addItemModal").hide();
@@ -136,49 +265,118 @@ function startItAll() {
         }
     });
 
+    $("#editModalItemSubmit").click(function () {
+        if (!validateName($("#editModalItemName").val())) {
+            //TODO: Handle bad name input
+        } else if (!validateQuantity($("#editModalItemQuantity").val())) {
+            //TODO: Handle bad quantity input
+        } else {
+            socket.emit("editItem", //id, name, quantity, comments, priority
+                group,
+                $("#storeItemID").val(),
+                cleanString($("#editModalItemName").val()),
+                $("#editModalItemQuantity").val(),
+                $("#editModalItemComment").val(),
+                $("#editModalItemPriority").is(":checked")
+            );
+            $("#mainView").show();
+            $("#editItemModal").hide();
+            clearAllInputFields();
+        }
+    });
+
     $("#createGroupButton").click(function () {
-        group = $("#groupSelector").val();
-        //group = $("#changeGroupText").val();
-        console.log(cleanString(group));
-        //socket.emit("getAllItems");
-        socket.emit("getGroupItems", cleanString(group));
+        let temp = $("#groupSelector").val().toLowerCase();
+        if (temp == '') {
+            group = 'test_group';
+        }
+        else {
+            group = cleanString(temp);
+        }
+        socket.emit("changeRoom", group);
+        //socket.emit("getGroupItems", group);
         //TODO: handle the group value
         $("#changeGroupModal").hide();
         $("#mainView").show();
-        $("#changeGroupText").val("");
     });
 
-    /*$("#createGroupButton").click(function () { // "GO" in change group modal
-        //TODO: handle the group value
+    $("#generateGroupButton").click(function () {
+        // group = prompt("Please enter a new group name");
+        // group = cleanString(group);
+        // //TODO: handle the group generation
+        // socket.emit("changeRoom", group);
+        // socket.emit("addNewGroup", group);
+        //socket.emit("getGroupItems", cleanString(group));
+        $("#createNewGroupModal").show();
         $("#changeGroupModal").hide();
-        $("#mainView").show();
-        $("#changeGroupText").val("");
-    });*/
-
-    $("#generateGroupButton").click(function () { // "Generate group" button in modal window
-        //TODO: handle the group generation
-        $("#changeGroupModal").hide();
-        $("#mainView").show();
-        $("#changeGroupText").val("");
+        $("#mainView").hide();
+        $("#createNewGroupInput").val('');
     });
 
-    $("#changeGroupButton").click(function () { // Button that displays the change group modal
+    $("#confirmCreateNewGroupButton").click(function() {
+        let newGroup = cleanString($("#createNewGroupInput").val());
+        //TODO: validate the newGroup variable
+        if (true) {
+            group = newGroup;
+            socket.emit("changeRoom", group);
+            socket.emit("addNewGroup", group);
+            socket.emit("getGroupItems", cleanString(group));
+            $("#createNewGroupModal").hide();
+            $("#mainView").show();
+        } else {
+            $("#validateNewGroupDiv").show();
+        }
+       
+
+    });
+
+    $("#changeGroupButton").click(function () {
+        socket.emit("getGroups");
         $("#changeGroupModal").show();
+        $("#changeGroupText").val('');
         $("#mainView").hide();
         $("#addItemModal").hide();
     });
+
+    
 
     $("#editItemCancel").click(function () {
         $("#mainView").show();
         $("#editItemModal").hide();
     });
 
-    $("#editModalItemSubmit").click(function () {
-        //TODO: Update values of Item
-        $("#mainView").show();
-        $("#editItemModal").hide();
+    $("#removeAllButton").click(function () {
+        $("#confirmDeleteAllModal").show();
+        $("#mainView").hide();
+    });
+
+
+    $("#table-quantity").click(function () {
+        currentSort = sortingType.quantity;
+        $("#table-name").css('background-color', '#90AFC5');
+        $("#table-priority").css('background-color', '#90AFC5');
+        $("#table-quantity").css('background-color', '#66a0c9');
+        updateGUI(items);
+    });
+
+    $("#table-name").click(function () {
+        currentSort = sortingType.name;
+        $("#table-name").css('background-color', '#66a0c9');
+        $("#table-priority").css('background-color', '#90AFC5');
+        $("#table-quantity").css('background-color', '#90AFC5');
+        updateGUI(items);
+    });
+
+    $("#table-priority").click(function () {
+        currentSort = sortingType.priority;
+        $("#table-name").css('background-color', '#90AFC5');
+        $("#table-priority").css('background-color', '#66a0c9');
+        $("#table-quantity").css('background-color', '#90AFC5');
+        updateGUI(items);
     });
 }
+
+
 
 function getModalItemPriority() {
     return $("#modalItemPriority").prop('checked');
--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var ObjectID = mongodb.ObjectID;
 var client = new MongoClient("mongodb://localhost:27017", { useNewUrlParser: true });
 var db;
 
-var groups = [];
+var groupArray = [];
+
+var clientGroup;
 
 var express = require("express");
 
@@ -21,94 +23,212 @@ var io = socketio(server);
 
 app.use(express.static("pub"));
 
-function sendItemListToClient(err, res) {
+
+/*function sendItemListToClient(err, res) {
 	console.log("Sending item list to client");
-	db.collection("items").find({}).toArray(function(err, docs) {
-		if (err!=null) {
+	io.emit("forceClientCall", 'forcing');
+	// db.collection("items").find({}).toArray(function(err, docs) {
+	// 	if (err!=null) {
+	// 		console.log("ERROR: " + err);
+	// 	}
+	// 	else {
+	// 		io.emit("updateItemList", docs);
+	// 	}
+	// });
+}*/
+
+function sendGroupListToClient() {
+	db.listCollections().toArray(function(err, cols) {
+		if(err != null) {
 			console.log("ERROR: " + err);
 		}
 		else {
-			io.emit("updateItemList", docs);
+			//groupArray = cols;
+			cols.sort(compareGroups);
+			io.emit("updateGroupList", cols);
 		}
 	});
 }
 
-io.on("connection", function(socket) {
+io.sockets.on("connection", function(socket) {
 	console.log("Somebody connected...");
 
-	socket.on("getGroupItems", function() {											//"Request Refresh Call"
-		db.collection("items").find({}).toArray(function(err, docs) {
-			if (err!=null) {
+
+	socket.room = 'test_group';
+	socket.join('test_group');
+
+
+
+	socket.on("getGroupCollections", function() {
+		db.listCollections().toArray(function(err, cols) {
+			if(err != null) {
 				console.log("ERROR: " + err);
 			}
 			else {
-				socket.emit("updateItemList", docs);
+				//groupArray = cols;
+				cols.sort(compareGroups);
+				io.emit("updateGroupList", cols);
 			}
 		});
 	});
 
-	socket.on("togglePriority", function(id, priority) {
-		console.log(oppositeBool(priority));
-		db.collection("items").updateOne({_id: ObjectID(id)}, { $set: { priority: oppositeBool(priority)}}, sendItemListToClient);
-	});
-
-	socket.on("togglePurchased", function(id, purchased) {
-		console.log("Toggling the purchased field of " + id + "and purchased should become "+ oppositeBool(purchased));
-		db.collection("items").updateOne({_id: ObjectID(id)}, { $set: { purchased: oppositeBool(purchased) }}, sendItemListToClient);
-	});
-
-	/*socket.on("purchaseItem", function(id, purchased) {
-		console.log("Item was purchased");
-		db.collection("items").updateOne({_id: ObjectID(id)}, { $set: { purchased: purchased }});
-	});
-	socket.on("unPurchaseItem", function(id, purchased) {
-		console.log("Item was un-purchased");
-		db.collection("items").updateOne({_id: ObjectID(id)}, { $set: { purchased: purchased }});
+	/*socket.on("getGroups", function() {
+		db.collection("items").find({name: "group_entry"}, {projection: { _id: 0, groupid: 1}}).toArray(function(err, docs) {
+			if(err != null) {
+				console.log("ERROR: " + err);
+			}
+			else {
+				groupArray = docs;
+				console.log(groupArray);
+				socket.emit("updateGroupList", docs);
+			}
+		});
 	});*/
+
+	socket.on("addNewGroup", function(newGroupFromClient) {
+		clientGroup = newGroupFromClient
+		db.createCollection(newGroupFromClient, sendGroupListToClient);
+	});
+
+/*	socket.on("createGroupEntry", function(newGroupFromClient) {
+		clientGroup = newGroupFromClient;
+		let objToInsert = {
+			name: "group_entry",
+			priority: false,
+			groupid: newGroupFromClient,
+			date: Date(),
+			quantity: 0,
+			purchased: false,
+			comments: "Group entry for memory"
+		}
+		db.collection("items").insertOne(objToInsert, sendItemListToClient);
+	});*/
+
+	socket.on("getGroupItems", function(group) {
+		console.log("Fetching the items for " + group);
+		db.collection(group).find({}).toArray(function(err, docs) {
+			console.log(docs);
+			if (err!=null) {
+				console.log("ERROR: " + err);
+			}
+			else {
+				io.in(socket.room).emit("updateItemList", docs);
+			}
+		});
+	});
+
+	socket.on("togglePriority", function(group, id, priority) {
+		clientGroup = group;
+		console.log(oppositeBool(priority));
+		db.collection(group).updateOne({_id: ObjectID(id)}, { $set: { priority: oppositeBool(priority)}}, function() {
+			io.in(socket.room).emit("forceClientCall");
+		});
+	});
+
+	socket.on("togglePurchased", function(group, id, purchased) {
+		console.log(group + " " + id + " " + purchased);
+		clientGroup = group;
+		console.log("Toggling the purchased field of " + id + "and purchased should become "+ oppositeBool(purchased));
+		db.collection(group).updateOne({_id: ObjectID(id)}, { $set: { purchased: oppositeBool(purchased) }}, function() {
+			io.in(socket.room).emit("forceClientCall");
+		});
+	});
+
+	
 
 	socket.on("receiveItemFromClient", function(group, name, quantity, comments, priority) {
 		let objToInsert = {
 			name: name,
 			priority: priority,
 			groupid: group,
-			date: Date(),
+			date: new Date(),
 			quantity: quantity,
 			purchased: false,
 			comments: comments
 		}
-		db.collection("items").insertOne(objToInsert, sendItemListToClient);
+		db.collection(group).insertOne(objToInsert, function() {
+			io.in(socket.room).emit("forceClientCall");
+		});
 		//insertNewItem("items", objToInsert);
 		console.log("item inserted");
 		//db.close();
 	});
 
-	socket.on("editItem", function(id, name, quantity, comments, priority) {
+	socket.on("editItem", function(group, id, name, quantity, comments, priority) {
+		clientGroup = group;
 		console.log(id + " "+name+" "+quantity+" "+comments+" "+priority);
-		db.collection("items").updateOne({_id: ObjectID(id)}, {$set: {
+		db.collection(group).updateOne({_id: ObjectID(id)}, {$set: {
 			name: name,
 			quantity: quantity,
 			comments: comments,
 			priority: priority
-		}}, 
-			sendItemListToClient);
+		}}, function() {
+			io.in(socket.room).emit("forceClientCall");
+		});
 		console.log("Item updated");
 	});
 
-	socket.on("deleteItem", function(id) {
-		db.collection("items").removeOne({_id: ObjectID(id)}, sendItemListToClient);
+	/*socket.on("deleteItem", function(group, id) {
+		clientGroup = group;
+		db.collection(group).removeOne({_id: ObjectID(id)}, sendItemListToClient);
+	});*/
+
+	socket.on("removePurchased", function(group) {
+		clientGroup = group;
+		console.log("Removing the purchased items for " + group);
+		console.log(group);	
+		//db.collection(group).deleteMany({purchased: true}, sendItemListToClient);
+		db.collection(group).deleteMany({purchased: true}, function() {
+			io.in(socket.room).emit("forceClientCall");
+		});
 	});
+
+	socket.on("deleteGroup", function(group) {
+		clientGroup = group;
+		db.collection(group).drop(sendGroupListToClient);
+		io.in(socket.room).emit("forceOutOfList");
+	});
+
+
+	socket.on("deleteItem", function(group, id) {
+		clientGroup = group;
+		db.collection(group).removeOne({_id: ObjectID(id)}, function() {
+			io.in(socket.room).emit("forceClientCall");
+		});
+	});
+
+	/*socket.on("deleteGroup", function(group) {
+		db.collection("items").remove({groupid: group}, sendGroupListToClient);
+	});*/
 
 	socket.on("disconnect", function() {
 		console.log("Somebody disconnected.");
 	});
+
+	socket.on("changeRoom", function(newRoom) {
+		socket.leave(socket.room);
+		socket.room = newRoom;
+		socket.join(socket.room);
+		console.log(io.sockets.adapter.rooms);
+		socket.emit("forceClientCall", 'forcing a call to the update function');
+		console.log("Current room: " + socket.room);
+	});
 	
 });
 
-function findAll(collection) {
-	db.collection(collection).find({}).toArray(function(err, result) {
+function compareGroups(a,b) {
+	if (a.name < b.name)
+	  return -1;
+	if (a.name > b.name)
+	  return 1;
+	return 0;
+}
+
+function findAll(collection, group) {
+	db.collection(collection).find({groupid: group}).toArray(function(err, result) {
 		if(err) throw err;
-		//console.log(result);
-		client.close();
+		console.log(result);
+		//client.close();
 	});
 }
 
@@ -117,7 +237,7 @@ function insertNewItem(collection, objToInsert) {
 		if (err) throw err;
 		//console.log("1 item inserted");
 		//client.close();
-	})
+	});
 }
 
 function oppositeBool(bool) {
@@ -126,6 +246,7 @@ function oppositeBool(bool) {
 	else
 		return true;
 }
+
 
 client.connect(function(err) {
 	if (err != null) throw err;
